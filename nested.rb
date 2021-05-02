df5243bedb25412bd833472ba591fc533fd5a669
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-05-02 21:55:42 +1000

    assignment submitted

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+  
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_key][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,14 +85,16 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto[:languages] = ["LISP", "C"]
+    programmer_hash
 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
 
-	programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -103,6 +109,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
+
+    alans_new_info = "GUI"
+
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 
 end
 
@@ -125,5 +136,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 end
